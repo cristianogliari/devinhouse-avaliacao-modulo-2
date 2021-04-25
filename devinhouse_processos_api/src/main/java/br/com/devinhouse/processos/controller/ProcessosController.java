5aacdd646cc04284517ab7622612d447ecb47325
Cristian Ogliari <cogliari@gmail.com>
2021-04-24 21:50:10 -0300

    add: url version control

--- devinhouse_processos_api/src/main/java/br/com/devinhouse/processos/controller/ProcessosController.java
+++ devinhouse_processos_api/src/main/java/br/com/devinhouse/processos/controller/ProcessosController.java
@@ -30,38 +30,38 @@ public class ProcessosController {
 	private ProcessoService service;
 	
 	// endpoint de cadastro
-	@RequestMapping(value = "/cadastrar", method = POST, consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
+	@RequestMapping(value = "/v1/cadastrar", method = POST, consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
 	@ResponseStatus(HttpStatus.CREATED)
 	public Processo registerProcess(@RequestBody Processo processo) {
 		return service.create(processo);
 	}
 	
 	// endpoint de consulta de todos os processos
-	@RequestMapping(value = "/consultar", method = GET, produces = APPLICATION_JSON_VALUE)
+	@RequestMapping(value = "/v1/consultar", method = GET, produces = APPLICATION_JSON_VALUE)
 	public List<Processo> allProcess() {
 		return service.getAllProcess();
 	}
 	
 	// endpoint de consulta de processos por id
-	@RequestMapping(value = "/consultar/id/{id}", method = GET, produces = APPLICATION_JSON_VALUE)
+	@RequestMapping(value = "/v1/consultar/id/{id}", method = GET, produces = APPLICATION_JSON_VALUE)
 	public Processo processById(@PathVariable Integer id) {
 		return service.getProcessByID(id);
 	}
 	
 	// endpoint de consulta de processos pela chave do processo
-	@RequestMapping(value = "/consultar/chaveprocesso", method = GET, produces = APPLICATION_JSON_VALUE)
+	@RequestMapping(value = "/v1/consultar/chaveprocesso", method = GET, produces = APPLICATION_JSON_VALUE)
 	public Processo processByKey(@RequestParam("chaveprocesso") String key) {
 		return service.getProcessByKey(key);
 	}
 	
 	// endpoint para atualizacao de todos os atributos de um processo
-	@RequestMapping(value = "/atualizar/id/{id}", method = PUT, consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
+	@RequestMapping(value = "/v1/atualizar/id/{id}", method = PUT, consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
 	public List<Processo> updateProcessAttribute(@PathVariable Integer id, @RequestBody Processo newObj){
 		return service.updateProcess(id, newObj);
 	}
 	
 	// endpoint de remocao de processos
-	@RequestMapping(value = "/remover/id/{id}", method = DELETE)
+	@RequestMapping(value = "/v1/remover/id/{id}", method = DELETE)
 	@ResponseStatus(HttpStatus.ACCEPTED)
 	public void removeProcess(@PathVariable Integer id) {
 		service.removeProcess(id);
