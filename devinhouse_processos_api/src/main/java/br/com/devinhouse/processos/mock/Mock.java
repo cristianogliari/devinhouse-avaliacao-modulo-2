059023d1f6430823c191f9cfd3c8cf690c056a5d
Cristian Ogliari <cogliari@gmail.com>
2021-04-24 22:34:36 -0300

    add: generate data for h2 to test

---
+++ devinhouse_processos_api/src/main/java/br/com/devinhouse/processos/mock/Mock.java
@@ -0,0 +1,43 @@
+package br.com.devinhouse.processos.mock;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+import br.com.devinhouse.processos.entity.Processo;
+import br.com.devinhouse.processos.repository.ProcessoRepository;
+
+@Configuration
+public class Mock implements CommandLineRunner {
+	
+	@Autowired
+	private ProcessoRepository repository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		List<Processo> allProcess = new ArrayList<Processo>();
+		int numProcess = 40;
+		
+		for(int i = 0; i < numProcess; i++) {
+			Processo process = new Processo();
+			
+			process.setCdAssunto(i+1);
+			process.setCdInteressado(i+1);
+			process.setDescricao("Processo " + (i + 1) + " de testes DEVinHouse");
+			process.setDescricaoAssunto("Assunto " + (i + 1) + " de testes DEVinHouse");
+			process.setNmInteressado("Cristian " + i);
+			process.setNuAnoProcesso(i <= 20? "2020" : "2021");
+			process.setNuProcesso(i+1);
+			process.setSgOrgaoProcesso("SOFT");
+			process.setChaveProcesso(process.getSgOrgaoProcesso() + " " + process.getNuProcesso() + "/" + process.getNuAnoProcesso());
+			allProcess.add(process);
+		}
+		
+		for(Processo process : allProcess) {
+			repository.save(process);
+		}	
+	}		
+}
